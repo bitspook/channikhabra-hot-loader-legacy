f2550a2e8743bfcdfa0621f12e8ce3c3ba6f78fb
Charanjit Singh <ckhabra007@gmail.com>
2015-05-16 21:33:13 +0530

    Add watcher for watching for new changes and hot-pushing them
    The problem with running a watcher inside meteor app is that then the watcher is as good as the meteor's refresh speed. i.e the new changes are pushed after meteor is done refreshing the client.
    
    I need to figure a way to disable this watcher if:
    - External watcher is installed
    - Some editor plugin does the pushing job
    
    Detecting the editor plugin won't be possible I believe. So, I think I should make it configurable to allow internal watcher. i.e make it opt in. I will now implement a configuration for `HotLoader` like:
    
    `HotLoader.enableWatch()` or may be `HotLoader.configure({watch: true})`

--- package.js
+++ package.js
@@ -10,6 +10,7 @@ Npm.depends({watch: "0.16.0"});
 
 Package.onUse(function(api) {
   api.use([
+    "http",
     "nucleuside:live-update@0.3.1",
   ]);
   api.use('iron:router@0.9.0 || 1.0.0', 'server');
@@ -19,6 +20,7 @@ Package.onUse(function(api) {
   ]);
 
   api.addFiles([
+    'lib/watcher/watcher.js',
     'lib/dispatcher/server/routes.js',
     'lib/dispatcher/server/pubs.js'
   ], 'server');
